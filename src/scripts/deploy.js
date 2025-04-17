5fbac3b95ccfc5f2fc1ed82003b9158c41a34c6c
dragonyc1002 <mivck1012@gmail.com>
2025-04-17 14:34:24 +0800

    chore: publishing command deployment script

---
+++ src/scripts/deploy.js
@@ -0,0 +1,39 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v10');
+
+const fs = require('fs');
+
+const { cmdGuilds } = require('../data/constants');
+const { bot } = require('../../config');
+
+const { token, id } = bot;
+
+const commands = [];
+
+const commandFiles = fs.readdirSync('./src/cmds')
+  .filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles){
+  const command = require(`../cmds/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({
+  version: '10'
+}).setToken(token);
+
+(async () => {
+  try {
+    console.log('Started deploying application commands...');
+  
+    await rest.put(
+      Routes.applicationCommands(id, cmdGuilds),
+      { body: commands }
+    );
+  } catch(err){
+    console.log(err);
+  }
+})();
+
+// for now, commands are only available for specified guilds as this bot is for private use
+// guilds where the commands are available are listed in constants.json
